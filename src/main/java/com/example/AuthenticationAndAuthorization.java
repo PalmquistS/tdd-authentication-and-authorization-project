07d5ed6cef7f8897143ba0ccfffb2aacbe41e29e
PalmquistS <71629477+PalmquistS@users.noreply.github.com>
2021-11-15 21:40:11 +0100

    Step 2, added list of users and made some changes in loggIn method

--- src/main/java/com/example/AuthenticationAndAuthorization.java
+++ src/main/java/com/example/AuthenticationAndAuthorization.java
@@ -1,8 +1,27 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AuthenticationAndAuthorization {
+    User user;
+    List<User> userList = new ArrayList<>();
+
+    public void addUser(String name, String password) {
+        user = new User(name, password);
+        userList.add(user);
+    }
 
     public boolean loggIn(String name, String password) {
-        return name.equals("anna") && password.equals("losen");
+        boolean isTrue = false;
+        for (User value : userList) {
+            if (name.equals(value.getName())) {
+                if (password.equals(value.getPassword())) {
+                    isTrue = true;
+                    break;
+                }
+            }
+        }
+        return isTrue;
     }
 }
--- src/test/java/com/example/AuthenticationAndAuthorizationTest.java
+++ src/test/java/com/example/AuthenticationAndAuthorizationTest.java
@@ -1,15 +1,22 @@
 package com.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AuthenticationAndAuthorizationTest {
-
+    AuthenticationAndAuthorization authenticationAndAuthorization;
+    @BeforeEach
+    void setUp() {
+        authenticationAndAuthorization = new AuthenticationAndAuthorization();
+        authenticationAndAuthorization.addUser("anna", "losen");
+        authenticationAndAuthorization.addUser("bertil", "123456");
+        authenticationAndAuthorization.addUser("kalle", "password");
+    }
     @Test
     void test_logg_in_success() {
-        AuthenticationAndAuthorization authenticationAndAuthorization = new AuthenticationAndAuthorization();
         boolean isLoggedIn = authenticationAndAuthorization.loggIn("anna", "losen");
         assertTrue(isLoggedIn);
 
